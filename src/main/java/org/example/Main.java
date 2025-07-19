b123f6c5da98f3342d926da4924da5b5f0337529
Krevetko1324 <nikitaprototupe222@mail.ru>
2025-07-19 14:57:24 +0300

    razobral tipi dannih

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,47 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.List;
+
 public class Main {
+
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and goodbye PRESS F !");
+        //String s = " Hello ";
+        // String z = " Fucking ";
+        // int a = 10;
+        // double d = 30.5;
+        // float z = 32.5F;
+        // byte boba = 16;
+        // long lenya = 22;
+        // short mili = 2;
+        // char s = 'a';
+        // boolean bobs = false;
+        //char kilka = 56;
+        //System.out.println(String.valueOf(kilka));
+        //System.out.println(a + d + z + boba );
+        //int a = 4;
+        //int b = 6;
+        // System.out.println( a + b + "  " +  "Hello and goodbye PRESS F !");
+       // String skovorodka = "Оладьи";
+       // String skovorodka2 = new String("Оладьи");
+       // String skovorodka3 = "Оладьи";
+
+       // System.out.println(skovorodka);
+
+       // System.out.println(skovorodka2);
+
+       // System.out.println(skovorodka3);
+
+       // System.out.println(" Стринг пулл skovorodka1 " + System.identityHashCode(skovorodka) +
+              //  " string puck skovorodka3 " + System.identityHashCode(skovorodka3));
+
+        //System.out.println(skovorodka == skovorodka2);
+
+        //System.out.println(skovorodka == skovorodka3);
+       // int [] kolbaska;
+       // int [][] ziliboba;
+       // Object shashlyk = new Object();
+        // List <String> list;
+
     }
+
 }
