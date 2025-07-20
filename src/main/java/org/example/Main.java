ed5cfb3498af1f306d5be734e8af6fef8f531f0c
Krevetko1324 <nikitaprototupe222@mail.ru>
2025-07-20 05:01:09 +0300

    razobral tipi dannih

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,35 @@
 package org.example;
 
+import java.awt.*;
+import java.io.Console;
+
+import static com.sun.org.apache.xalan.internal.xsltc.compiler.Constants.RESET;
+import static java.awt.Color.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and goodbye PRESS F !");
+
+        //System.out.println("Hello and goodbye PRESS F !");
+        //Color color = SystemColor.getColor(String.valueOf(yellow));
+        //String s = "gamer";
+       // System.out.println(s);
+       // Color color1 = getColor(s);
+        double gamma = 20;
+        double beta = 30;
+        double delta = 2.5;
+        while (gamma != beta)
+        {
+            gamma += delta;
+            System.out.println(gamma);
+            System.out.println("нужно больше");
+        }
+
+        System.out.println("Okay, let's go");
+
     }
+
 }
